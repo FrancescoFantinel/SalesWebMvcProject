678a51a75234fc75dbe294b9c1a9f6e25c9b7d40
Francesco Fantinel <francescoacf@outlook.com>
2022-04-04 21:04:44 -0300

    CRUD Scaffolding

--- SalesWebMvcProject/Models/Department.cs
+++ SalesWebMvcProject/Models/Department.cs
@@ -1,4 +1,7 @@
 ﻿namespace SalesWebMvcProject.Models {
     public class Department {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- SalesWebMvcProject/Startup.cs
+++ SalesWebMvcProject/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcProject.Data;
 
 namespace SalesWebMvcProject {
     public class Startup {
@@ -28,6 +30,9 @@ namespace SalesWebMvcProject {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<SalesWebMvcProjectContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
