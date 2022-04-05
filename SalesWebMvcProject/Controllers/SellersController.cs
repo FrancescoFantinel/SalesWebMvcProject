caaac56973e0d87e306dad9415f202148b80b7f5
Francesco Fantinel <francescoacf@outlook.com>
2022-04-04 22:30:02 -0300

    SellersController

---
+++ SalesWebMvcProject/Controllers/SellersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvcProject.Controllers {
+    public class SellersController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
