3e661fc41d36b3d4cdec02d8e9f76fafd140faa7
Francesco Fantinel <francescoacf@outlook.com>
2022-04-05 21:01:13 -0300

    Simple Create Form

--- SalesWebMvcProject/Services/SellerService.cs
+++ SalesWebMvcProject/Services/SellerService.cs
@@ -13,5 +13,10 @@ namespace SalesWebMvcProject.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj) {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
