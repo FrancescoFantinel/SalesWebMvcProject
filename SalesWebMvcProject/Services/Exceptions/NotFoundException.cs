1d0e5c4ef88edb335fe05dff351a90a13b757a27
Francesco Fantinel <francescoacf@outlook.com>
2022-04-05 22:14:03 -0300

    Update seller and custom service exception

---
+++ SalesWebMvcProject/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SalesWebMvcProject.Services.Exceptions {
+    public class NotFoundException : ApplicationException {
+        public NotFoundException(string message) : base(message) {
+        }
+    }
+}
--- SalesWebMvcProject/Services/SellerService.cs
+++ SalesWebMvcProject/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvcProject.Services.Exceptions;
 
 namespace SalesWebMvcProject.Services {
     public class SellerService {
@@ -10,7 +11,7 @@ namespace SalesWebMvcProject.Services {
         public SellerService(SalesWebMvcProjectContext context) {
             _context = context;
         }
-        
+
         public List<Seller> FindAll() {
 
             return _context.Seller.ToList();
@@ -32,6 +33,19 @@ namespace SalesWebMvcProject.Services {
             _context.Seller.Remove(obj);
             _context.SaveChanges();
 
+        }
+
+        public void Update(Seller obj) {
+            if (_context.Seller.Any(x => x.Id == obj.Id)) {
+                throw new NotFoundException("Id not found");
+            }
+            try {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
