415389ccc54c395f437169cda1657784b80622aa
Francesco Fantinel <francescoacf@outlook.com>
2022-04-05 21:07:34 -0300

    Foreign key not null (referencial integrity)

--- SalesWebMvcProject/Services/SellerService.cs
+++ SalesWebMvcProject/Services/SellerService.cs
@@ -15,6 +15,8 @@ namespace SalesWebMvcProject.Services {
         }
 
         public void Insert(Seller obj) {
+
+            obj.Department= _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
